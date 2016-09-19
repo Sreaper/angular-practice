6e48cacea538e90aa394c368270496118e6717fa
Sreaper <songzhimaoszm@gmail.com>
2016-09-19 08:02:42 +0800

    test diractive

--- 1.1/test/js/practice_hello_directive.js
+++ 1.1/test/js/practice_hello_directive.js
@@ -1,3 +1,14 @@
 /**
  * Created by szm on 2016/9/11.
  */
+var szmModule = angular.module("szmHello", []);
+szmModule.controller("helloController", ["$scope", function ($scope) {
+	$scope.name = "szm";
+}])
+szmModule.directive("hello", function () {
+	return {
+		restrict: "E",
+		template: '<div> hello lyl<div',
+		replace: true
+	};
+});
--- 2.1/app/Scope2.js
+++ 2.1/app/Scope2.js
@@ -1,6 +1,15 @@
 function EventController($scope) {
 	$scope.count = 0;
-	$scope.$on('MyEvent', function() {
+	$scope.$on('MyEvent', function () {
 		$scope.count++;
+		console.log($scope.count);
+	});
+}
+
+function EventController1($scope) {
+	$scope.count = 0;
+	$scope.$on('MyEvent1', function () {
+		$scope.count++;
+		console.log($scope.count);
 	});
 }
